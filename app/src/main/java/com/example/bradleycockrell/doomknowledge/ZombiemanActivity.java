600c1c7da4bd93d2aa6da4e186ada6c64f47fb31
Bradly Cockrell <djbradlythehedgehog@gmail.com>
2018-06-26 13:19:39 -0400

    DoomKnowledge started

---
+++ app/src/main/java/com/example/bradleycockrell/doomknowledge/ZombiemanActivity.java
@@ -0,0 +1,16 @@
+package com.example.bradleycockrell.doomknowledge;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ZombiemanActivity  {
+
+    protected Button zombieman_correct_hp;
+
+    protected Button zombieman_incorrect_hp1;
+
+    protected Button zombieman_incorrect_hp2;
+
+    protected Button zombieman_incorrect_hp3;
+
+}
