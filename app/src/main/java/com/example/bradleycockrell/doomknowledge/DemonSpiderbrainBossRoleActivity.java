f4048e3645966a959397c8edd370cc72fd21707a
Bradly Cockrell <djbradlythehedgehog@gmail.com>
2018-06-27 13:21:30 -0400

    Try to figure out how to make the methods of Demon Spiderbrain purple

---
+++ app/src/main/java/com/example/bradleycockrell/doomknowledge/DemonSpiderbrainBossRoleActivity.java
@@ -0,0 +1,16 @@
+package com.example.bradleycockrell.doomknowledge;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DemonSpiderbrainBossRoleActivity {
+
+    protected Button demon_spiderbrain_incorrect_role3;
+
+    protected Button demon_spiderbrain_incorrect_role2;
+
+    protected Button demon_spiderbrain_incorrect_role1;
+
+    protected Button demon_spiderbrain_correct_role;
+
+}
